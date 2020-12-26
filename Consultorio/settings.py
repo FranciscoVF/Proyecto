102a610e546b10053e36bdf5f760578ce87c648b
FranciscoVF <francisco.villarreal16@tectijuana.edu.mx>
2020-12-25 23:42:07 -0800

    crispy agregado al forms

--- Consultorio/settings.py
+++ Consultorio/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'general',
     'Medicos',
 ]
@@ -140,3 +141,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
