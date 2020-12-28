5589b0840ede0d9da5ba7d6557d3bbf778bf6d39
FranciscoVF <francisco.villarreal16@tectijuana.edu.mx>
2020-12-28 12:41:24 -0800

    Modelo medico generado

--- Medicos/models.py
+++ Medicos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Medicos(models.Model):
+    medico = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
+    nombre = models.CharField(max_length=50, null = True)
+    apellido_p = models.CharField(max_length=50, help_text="Apellido paterno", null = True)
+    apellido_m = models.CharField(max_length=50, help_text="Apellido materno", null = True)
+    servicio = models.CharField(max_length=50, help_text="Especialidad")
+    entrada = models.IntegerField(null=True, help_text="Formato de 24 horas")
+    salida = models.IntegerField(null=True, help_text="Formato de 24 horas")
+    dias_de_trabajo = models.CharField(max_length=50, null=True, help_text="Todos los dias que se va trabajar")
+
+    def __str__(self):
+        return self.nombre
